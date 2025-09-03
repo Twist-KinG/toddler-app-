55c7aed5bba3dfcbcf8419943cbba008d8170567
Dipesh-Theeng-Tamang <dt412242@gmail.com>
2025-09-04 01:34:07 +0545

    Update capitalletters.jsx

--- client/src/components/alphabets/capitalletters.jsx
+++ client/src/components/alphabets/capitalletters.jsx
@@ -1,13 +1,15 @@
 // CapitalLetters.jsx
 import React, { useEffect, useRef } from 'react';
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom"; 
 import { MdChevronLeft } from "react-icons/md";
-import logo from "../../assets/img/Toodler.png"; // Importing the local logo image
-// import image from "../../assets/img/b.png"; // Importing the local logo image
+import logo from "../../assets/img/Toodler.png";
 
 const capitalletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
 const smallletter = "abcdefghijklmnopqrstuvwxyz".split("");
+
 const vowels = "AEIOU".split("");
+
 const numbers = [
   { id: '0', number: '0', name: 'Zero' },
   { id: '1', number: '1', name: 'One' },
@@ -22,26 +24,7 @@ const numbers = [
 ]
 
 const CapitalLetters = () => {
-  // Use a ref to store audio elements mapped by letter
-  // const audioMapRef = useRef({});
 
-  // useEffect(() => {
-  //   // Preload audio elements for each letter
-  //   const audioMap = {};
-  //   letters.forEach(letter => {
-  //     audioMap[letter] = new Audio(`https://actions.google.com/sounds/v1/letters/letter_${letter.toLowerCase()}.mp3`);
-  //   });
-  //   audioMapRef.current = audioMap;
-  // }, []);
-
-  // const playSound = (letter) => {
-  //   const audio = audioMapRef.current[letter];
-  //   if (audio) {
-  //     audio.pause();
-  //     audio.currentTime = 0;
-  //     audio.play();
-  //   }
-  // };
 
   return (
     <div className="bg-white mb-5 min-h-screen max-w-md flex flex-col items-center pt-8 px-6 font-indie relative">
