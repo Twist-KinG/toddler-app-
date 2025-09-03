0dfe2c9aa34d6ba56c0599e316ef8b969596b32b
Dipesh-Theeng-Tamang <dt412242@gmail.com>
2025-09-04 02:07:29 +0545

    add sounds

--- client/src/components/alphabets/capitalletters.jsx
+++ client/src/components/alphabets/capitalletters.jsx
@@ -1,15 +1,13 @@
-// CapitalLetters.jsx
-import React, { useEffect, useRef } from 'react';
-import { Link } from "react-router-dom"; 
+
+import React from 'react';
+import { Link } from "react-router-dom";
 import { MdChevronLeft } from "react-icons/md";
 import logo from "../../assets/img/Toodler.png";
 
-const capitalletter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-const smallletter = "abcdefghijklmnopqrstuvwxyz".split("");
-
+const capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const smallLetters = "abcdefghijklmnopqrstuvwxyz".split("");
 const vowels = "AEIOU".split("");
-
 const numbers = [
   { id: '0', number: '0', name: 'Zero' },
   { id: '1', number: '1', name: 'One' },
@@ -21,11 +19,15 @@ const numbers = [
   { id: '7', number: '7', name: 'Seven' },
   { id: '8', number: '8', name: 'Eight' },
   { id: '9', number: '9', name: 'Nine' },
-]
+];
+
+// Play sound function
+const playSound = (key) => {
+  const audio = new Audio(`/sounds/EnglishAlphabet/${key.toLowerCase()}.mp3`);
+  audio.play();
+};
 
 const CapitalLetters = () => {
-
-
   return (
     <div className="bg-white mb-5 min-h-screen max-w-md flex flex-col items-center pt-8 px-6 font-indie relative">
       <Link to="/learnalphabets">
@@ -36,36 +38,38 @@ const CapitalLetters = () => {
 
       {/* Logo */}
       <img
-        src={logo} // Using the imported logo image
+        src={logo}
         alt="Toddler Fun Learning logo with two kids and stars"
         className="w-30 h-30 mb-1"
         width={60}
         height={60}
       />
-      {/* Title */}
+
       <h1 className="text-purple-600 text-5xl leading-tight mb-0">English</h1>
       <p className="text-purple-600 text-3xl mb-6">Alphabets & Numbers</p>
+
+      {/* Numbers */}
       <span className='text-2xl text-white px-3 py-2 rounded-lg bg-purple-600 mb-5 mt-10'>Numbers</span>
       <div className="grid grid-cols-5 gap-[10px] w-full">
-          {numbers.map((num) => (
-            <button
-              key={num.id}
-              aria-label={`Play sound for letter ${num.number}`}
-              className="bg-purple-600 rounded-md text-center text-white w-[74px] h-[74px] font-indie transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
-              onClick={() => playSound(num.number)}
-              type="button"
-            >
-              <span className="text-4xl">{num.number}</span><br />
-              <span className="text-xl">{num.name}</span>
-            </button>
-          ))}
+        {numbers.map(num => (
+          <button
+            key={num.id}
+            className="bg-purple-600 rounded-md text-center text-white w-[74px] h-[74px] font-indie transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
+            onClick={() => playSound(num.number)}
+            type="button"
+          >
+            <span className="text-4xl">{num.number}</span><br />
+            <span className="text-xl">{num.name}</span>
+          </button>
+        ))}
       </div>
+
+      {/* Vowels */}
       <span className='text-2xl text-white px-3 py-2 rounded-lg bg-purple-600 mb-5 mt-10'>Vowels</span>
       <div className="grid grid-cols-5 justify-center gap-[10px] w-full">
-        {vowels.map((letter) => (
+        {vowels.map(letter => (
           <button
             key={letter}
-            aria-label={`Play sound for letter ${letter}`}
             className="bg-purple-600 rounded-md py-4 text-4xl text-white w-[74px] h-[74px] font-normal transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
             onClick={() => playSound(letter)}
             type="button"
@@ -74,12 +78,13 @@ const CapitalLetters = () => {
           </button>
         ))}
       </div>
+
+      {/* Capital Letters */}
       <span className='text-2xl text-white px-3 py-2 rounded-lg bg-purple-600 mb-5 mt-10'>Capital Letters</span>
       <div className="grid grid-cols-5 justify-center gap-[10px] w-full">
-        {capitalletter.map((letter) => (
+        {capitalLetters.map(letter => (
           <button
             key={letter}
-            aria-label={`Play sound for letter ${letter}`}
             className="bg-purple-600 rounded-md py-4 text-4xl text-white w-[74px] h-[74px] font-normal transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
             onClick={() => playSound(letter)}
             type="button"
@@ -88,12 +93,13 @@ const CapitalLetters = () => {
           </button>
         ))}
       </div>
+
+      {/* Small Letters */}
       <span className='text-2xl text-white px-3 py-2 rounded-lg bg-purple-600 mb-5 mt-10'>Small Letters</span>
       <div className="grid grid-cols-5 justify-center gap-[10px] w-full">
-        {smallletter.map((letter) => (
+        {smallLetters.map(letter => (
           <button
             key={letter}
-            aria-label={`Play sound for letter ${letter}`}
             className="bg-purple-600 rounded-md py-4 text-4xl text-white w-[74px] h-[74px] font-normal transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
             onClick={() => playSound(letter)}
             type="button"
